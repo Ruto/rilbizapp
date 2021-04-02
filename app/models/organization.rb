b24b30dadcf795d1274764c77072a5242a1fd8ba
Ken Ruto <ruto.k.ken@gmail.com>
2021-04-02 10:20:20 +0300

    Polymorphic fix

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,7 @@
 class Organization < ApplicationRecord
   has_ancestry
-  belongs_to :organizable, polymorphic: true
+  belongs_to :organizable, polymorphic: true, optional: true
   belongs_to :user
+  has_many :structures, as: :structurable
+
 end
--- app/models/structure.rb
+++ app/models/structure.rb
@@ -2,4 +2,5 @@ class Structure < ApplicationRecord
   has_ancestry
   belongs_to :structurable, polymorphic: true
   belongs_to :user
+  has_many :organizations, as: :organizable
 end
